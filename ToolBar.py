8dd4dfd0a4dfb258c342130d73f7a024a38dded7
Neo1289 <noam81@yahoo.it>
2025-01-12 19:01:33 +0100

    added toolbar logic

--- ToolBar.py
+++ ToolBar.py
@@ -7,16 +7,11 @@ from game_settings import (display_surface,
                            button_color,
                            sys)
 from sprites import GroundSprite,CollisionSprite,AreaSprite
-from player import Player
-from groups import allSprites
-from enemy import Enemy,frames
 
-class SideGame():
+class ToolBar():
     def __init__(self):
         self.running = True
         self.clock = pygame.time.Clock()
-        self.all_sprites = allSprites()
-        self.collision_sprites = pygame.sprite.Group()
 
     def run(self):
         dt = self.clock.tick() / 10000
@@ -24,13 +19,12 @@ class SideGame():
         while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                     self.running = False
-                if event.type == pygame.KEYDOWN and event.key == pygame.K_m:
-                    pass
 
             display_surface.fill((0, 0, 0))
-            self.all_sprites.update(dt)
             pygame.display.update()
 
-        pygame.quit()
-        sys.exit()
+        return
