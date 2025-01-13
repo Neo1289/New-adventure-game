3cfeb73724d6db71086c746068ffb2c437d2b518
Neo1289 <noam81@yahoo.it>
2025-01-13 16:33:30 +0100

    working on the transition

--- ToolBar.py
+++ ToolBar.py
@@ -5,26 +5,30 @@ from game_settings import (display_surface,
                            FONT_SIZE,
                            WINDOW_WIDTH,WINDOW_HEIGHT,
                            button_color,
-                           sys)
+                           sys,
+                           join
+                           )
 from sprites import GroundSprite,CollisionSprite,AreaSprite
 
 class ToolBar():
     def __init__(self):
         self.running = True
         self.clock = pygame.time.Clock()
+        self.image = pygame.image.load(join('resources','world','tool.png')).convert_alpha()
 
     def run(self):
-        dt = self.clock.tick() / 10000
 
         while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+
                 if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                     self.running = False
 
-            display_surface.fill((0, 0, 0))
+            display_surface.fill('white')
+            display_surface.blit(self.image,(0,0))
             pygame.display.update()
 
         return
