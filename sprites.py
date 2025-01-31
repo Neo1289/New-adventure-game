ca540e1369adb0c3560134534c19267d101147f0
Neo1289 <noam81@yahoo.it>
2025-01-31 16:22:06 +0100

    changed stuff around

--- sprites.py
+++ sprites.py
@@ -8,7 +8,7 @@ class GroundSprite(pygame.sprite.Sprite):
          self.ground = True
 
 class CollisionSprite(pygame.sprite.Sprite):
-    def __init__(self, pos, surf, groups,name):
+    def __init__(self, pos, surf, groups, name):
         super().__init__(groups)
         self.image = surf
         self.rect = self.image.get_rect(topleft = pos)
@@ -20,6 +20,14 @@ class AreaSprite(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = pygame.Surface((width, height), pygame.SRCALPHA)
         self.rect = pygame.Rect(x, y, width, height)
+
+
+class InventorySprite(pygame.sprite.Sprite):
+    def __init__(self, width, height):
+        super().__init__()
+        self.image = pygame.Surface((100, 100))
+        self.image.fill((139,69,19))  # Fill with brown color
+        self.rect = self.image.get_rect(center=(width + 100, height + 100))
 
 
 
