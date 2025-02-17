17e1d0d9a0c4af9e1746df1d238a8cfdca585a77
noam emilio radetich <noam81@yahoo.it>
2025-02-17 16:25:08 +0100

    added some changes

--- side_game.py
+++ side_game.py
@@ -10,7 +10,6 @@ from game_settings import (display_surface,
                            )
 from player_side_game import PlayerSide
 
-
 class SideGame():
     def __init__(self):
         self.running = True
@@ -19,7 +18,7 @@ class SideGame():
         self.display_surface = display_surface
 
     def run(self):
-        dt = self.clock.tick(600)
+        dt = self.clock.tick(60)
         while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -28,10 +27,9 @@ class SideGame():
                 if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                     self.running = False
 
-
             self.display_surface.fill((0, 0, 0))
             self.player.update(dt)
-            self.display_surface.blit(self.player.image, (WINDOW_HEIGHT / 2, WINDOW_WIDTH / 2))
+            self.display_surface.blit(self.player.image, self.player.rect)
             pygame.display.update()
 
         return
