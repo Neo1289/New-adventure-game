5b442630deb2cd73c090455c668e6d866e338b18
noam emilio radetich <noam81@yahoo.it>
2025-04-08 17:03:12 +0200

    progressing with the side_game

--- side_game.py
+++ side_game.py
@@ -20,8 +20,23 @@ class SideGame():
         self.display_surface = display_surface
         self.platform_group = pygame.sprite.Group()
         self.collision_sprites = pygame.sprite.Group()
-        self.platform_player = Platform((100, 500), self.platform_group, self.collision_sprites)
-        self.ground_rect = pygame.Rect(0, 500, self.display_surface.get_width(), 50)
+        self.platform_player = Platform((100, self.display_surface.get_height()-50), self.platform_group, self.collision_sprites)
+        self.ground_rect = pygame.Rect(0,self.display_surface.get_height() - 50, self.display_surface.get_width(), 50)
+        self.WHITE = (255, 255, 255)
+        self.BLUE = (0, 0, 255)
+        self.GREEN = (0, 255, 0)
+
+        self.platforms = [
+            [40, self.display_surface.get_height() - 50, self.display_surface.get_width(), 20],
+            [100, 400, 200, 20],
+            [400, 300, 150, 20],
+            [250, 200, 100, 20],
+            [550, 150, 200, 20]
+        ]
+
+    def draw_platforms(self):
+        for platform in self.platforms:
+            pygame.draw.rect(self.display_surface, self.GREEN, platform)
 
     def run(self):
 
@@ -42,6 +57,7 @@ class SideGame():
             self.display_surface.fill((0, 0, 0))
             # Draw ground
             pygame.draw.rect(self.display_surface, (100, 50, 0), self.ground_rect)
+            self.draw_platforms()
             self.platform_group.update(dt)
             self.platform_group.draw(self.display_surface)
 
